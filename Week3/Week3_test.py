5d68d8d0a171c869df3cddb6bf1802245bbf3c0d
PoppyFP <Forshaw_Perring-P@ulster.ac.uk>
2025-03-27 21:38:19 +0000

    Update Week3_test.py
    Linestring section done

--- Week3/Week3_test.py
+++ Week3/Week3_test.py
@@ -16,3 +16,29 @@ shapely.get_coordinates(pt) #get coordinates for points; can also be done with j
 
 pt_buffer = pt.buffer(0.001) # buffer the point by 0.001 in the same coordinates
 print(type(pt_buffer)) # show the type of the buffer
+
+line1 = LineString([pt, pt2]) # method one of creating a LineString, using a list of Point objects
+line2 = LineString([(-6.677, 55.150), (-6.658, 55.213)]) # method two, using a list of coordinate tuples
+
+print(line1) # show the first line
+print(line2) # show the second line
+
+print(line1==line2) # check to see if these are the same geometry
+
+print(line1.xy[0])
+print(line1.xy[1])
+
+x, y = line1.xy
+
+print(x)
+print(y)
+
+print(line1.length)
+
+center = line1.centroid # get the midpoint of the line
+print(center)
+
+line1.project(center) / line1.length # check to see how far along the line our centerpoint is
+
+print(center) # print the WKT representation of the center point
+print(line1.interpolate(0.5, normalized=False)) # print the WKT representation of the point 50% along the line
