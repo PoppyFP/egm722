41c715c61db3933a1921d6b6a0fc9d050744d9d4
PoppyFP <Forshaw_Perring-P@ulster.ac.uk>
2025-03-27 21:00:29 +0000

    Update Week3_test.py
    get_coordinates notes added, and pt.buffer tested

--- Week3/Week3_test.py
+++ Week3/Week3_test.py
@@ -12,4 +12,7 @@ pt3d = Point(86.925278, 27.988056, 8848.86) # creates a 3d point
 
 print(pt) # print a well-known text (WKT) representation of the Point object
 
-shapely.get_coordinates(pt)
+shapely.get_coordinates(pt) #get coordinates for points; can also be done with just dir(pt)?
+
+pt_buffer = pt.buffer(0.001) # buffer the point by 0.001 in the same coordinates
+print(type(pt_buffer)) # show the type of the buffer
